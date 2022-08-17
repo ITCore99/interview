9aa29cbd4b463de62343f570d2abc86a047ab6f9
fuzhuoning <fuzhuoning@aiforward.com>
2022-08-17 11:30:37 +0800

    完善算法

---
+++ 算法/合并两个有序数组.js
@@ -0,0 +1,21 @@
+/**
+ * 合并两个有数数组
+ * 注意我们在归并中也写了一个 合并有序数组(从头开始开始遍历) 那个是返回一个新数组 而我们本次的理解稍微复杂点(从尾部开始替换) 并且会修改arr1原数组
+ */
+function mergeOrderlyArr(arr1, arr2) {
+  let i = arr1.length - 1
+  let j = arr2.length - 1
+  let insertIndex =  arr1.length +  arr2.length - 1 // 从最后一个位置开始插入
+  while(j >= 0) {
+    if(i < 0) { // 说明数组1已经完毕
+      arr1[insertIndex--] = arr2[j--]
+    }
+    arr1[insertIndex--] =  arr1[i] > arr2[j] ? arr1[i--] :  arr2[j--]
+  }
+  return arr1
+}
+
+let num1 = [3, 5, 6, 7]
+let num2 = [1, 2, 3]
+const res = mergeOrderlyArr(num1, num2)
+console.log('res', res)
---
+++ 算法/排序-归并.js
@@ -0,0 +1,51 @@
+/**
+ * 归并排序 是一种分而治之的一种思想
+ * 将数组从中间分为两部分 再对左右进行递归的排序 等到左右两边有数数组之后 再进行合并
+ * 核心是如何对两个有序数组进行合并
+ * 
+ */
+
+// 生成随机数
+function generatorNum(num = 100, min = 0, max = num) {
+  const result = []
+  for(let i = 0; i < num; i++) {
+    result.push(Math.floor(Math.random() * ((max - min) + min)))
+  }
+  return result
+}
+const numArr = generatorNum()
+console.log('原始数据', numArr)
+
+// 归并排序算法
+function mergeSort(arr) {
+  if (arr.length < 2) return arr
+  const p = Math.floor(arr.length / 2)
+  const left = mergeSort(arr.slice(0, p))
+  const right = mergeSort(arr.slice(p))
+  return merge(left, right)
+}
+
+// 进行两个有序数组的合并
+function merge(source1, source2) {
+  let i = 0 // source1的指针
+  let j = 0 // source2的指针
+  const result = []
+  while(i < source1.length && j < source2.length) {
+    if (source1[i] < source2[j]) {
+      result.push(source1[i])
+      i++
+    } else {
+      result.push(source2[j])
+      j++
+    }
+  }
+  // 处理数组多余的数组
+  if(i < source1.length) {
+    result.push(...source1.slice(i))
+  } else {
+    result.push(...source2.slice(j))
+  }
+  return result
+}
+
+console.log('排完之后数据', mergeSort(numArr))
